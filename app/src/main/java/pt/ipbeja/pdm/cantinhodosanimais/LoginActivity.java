6e1b91b165a2a614f779c6ea9b7b1ce7309c0677
cumaio17338 <17338@stu.ipbeja.pt>
2021-01-16 16:35:56 +0000

    Inicio de login

---
+++ app/src/main/java/pt/ipbeja/pdm/cantinhodosanimais/LoginActivity.java
@@ -0,0 +1,14 @@
+package pt.ipbeja.pdm.cantinhodosanimais;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
