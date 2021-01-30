b5a1bb5049ceb820de914b1982e07ac467019ec4
cumaio17338 <17338@stu.ipbeja.pt>
2021-01-30 20:11:59 +0000

    Layouts Sign in signup completos

--- app/src/main/java/pt/ipbeja/pdm/cantinhodosanimais/LoginActivity.java
+++ app/src/main/java/pt/ipbeja/pdm/cantinhodosanimais/LoginActivity.java
@@ -1,14 +1,19 @@
 package pt.ipbeja.pdm.cantinhodosanimais;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+
+
     }
 }
