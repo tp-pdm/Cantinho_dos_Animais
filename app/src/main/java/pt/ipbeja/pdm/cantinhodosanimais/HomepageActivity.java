9345114ff5325d46a2413d736dd14951a290c4bc
jaciara <18582@stu.ipbeja.pt>
2021-01-30 19:48:35 +0000

    homepage- with tabs

---
+++ app/src/main/java/pt/ipbeja/pdm/cantinhodosanimais/HomepageActivity.java
@@ -0,0 +1,25 @@
+package pt.ipbeja.pdm.cantinhodosanimais;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import pt.ipbeja.pdm.cantinhodosanimais.ui.main.SectionsPagerAdapter;
+
+public class HomepageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+    }
+}
---
+++ app/src/main/java/pt/ipbeja/pdm/cantinhodosanimais/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,52 @@
+package pt.ipbeja.pdm.cantinhodosanimais.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import pt.ipbeja.pdm.cantinhodosanimais.AdotarFragmentoActivity;
+import pt.ipbeja.pdm.cantinhodosanimais.HomepageActivity;
+import pt.ipbeja.pdm.cantinhodosanimais.R;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        switch (position) {
+            case 0:
+                fragment = new AdotarFragmentoActivity();
+                break;
+        }
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 1;
+    }
+}
